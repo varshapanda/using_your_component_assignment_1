15ae23b41e0afc3baa163164d0e600c0b527a973
varshapanda <varsha.vijayashwini@kalvium.community>
2025-01-23 14:31:27 +0530

    Using your component to view all of the list of entities - Practice assignment #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BookCard from './components/bookcard';
 
 function App() {
   const books = [
@@ -34,7 +35,19 @@ function App() {
     },
   ];
 
-  return <>{/* write code to display all the books using */}</>;
+  return (
+    <div>
+      {books.map((book)=>(
+        <BookCard
+        key={book.id}
+        image={book.image}
+        name={book.name}
+        genre={book.genre}
+        author={book.author}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default App;
--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,17 @@
 // write the book component code here
+function BookCard({image, name, genre, author}){
+    return(
+        <div>
+            <img src={image} alt={name} />
+            <br/>
+            <b>{name}</b>
+            <br/>
+            <b>Genre:{genre}</b>
+            <br/>
+            <b>Author:{author}</b>
+            <br/>
+            <br/>
+        </div>
+    )
+}
+export default BookCard;
